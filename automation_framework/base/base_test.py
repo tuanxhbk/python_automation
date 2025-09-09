13e25d5a0cbb1e80a50f25913ca9c06d6bc31a00
Tuan <tuan@gmail.com>
2025-09-09 21:01:33 +0700

    update 2025/09/09

---
+++ automation_framework/base/base_test.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+from time import sleep
+
+class BaseTest:
+    @pytest.fixture(scope='class', autouse=True)
+    def setup(self, request):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        sleep(5)
+        request.cls.driver = driver
+        yield
+        driver.quit()
---
+++ automation_framework/tests/test_login_use_inheritance.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+import pytest
+from base.base_test import BaseTest
+from selenium import webdriver
+from time import sleep
+
+class TestOrangeHRM(BaseTest):
+    USERNAME_INPUT = (By.XPATH, "//input[@name='username']")
+    PASSWORD_INPUT = (By.XPATH, "//input[@name='password']")
+    LOGIN_BUTTON = (By.XPATH, "//button[@type='submit']")
+
+    def test_login(self):
+        username = self.driver.find_element(*self.USERNAME_INPUT)
+        username.send_keys("Admin")
+        password = self.driver.find_element(*self.PASSWORD_INPUT)
+        password.send_keys("admin123")
+        login_button = self.driver.find_element(*self.LOGIN_BUTTON)
+        login_button.click()
+        sleep(5)
