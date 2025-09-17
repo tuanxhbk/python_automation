46c04f5da3be5a7165785f5b49c6668b67377590
Tuan Nguyen <nguyenmanhtuanxh@gmail.com>
2025-09-18 00:28:09 +0700

    remove sleep

--- automation_framework/tests/test_vacancies.py
+++ automation_framework/tests/test_vacancies.py
@@ -2,7 +2,6 @@ from base.base_test import BaseTest
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 from pages.recruitment_page import RecruitmentPage
-from time import sleep
 from datetime import datetime
 
 
@@ -21,5 +20,3 @@ class TestVacancies(BaseTest):
         dashboard_page.navigate_to_recruitment()
         recruitment_page = RecruitmentPage(self.driver)
         recruitment_page.add_new_vacancy(vacancy_name, job_title, description, number_of_positions )
-
-        sleep(5)  # import time
