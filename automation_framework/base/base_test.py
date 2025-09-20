6a560ac21d065dd0afa994c619b153ba1e52ebf5
Your Name <you@example.com>
2025-09-20 19:30:50 +0700

    add headless

--- automation_framework/base/base_test.py
+++ automation_framework/base/base_test.py
@@ -5,7 +5,7 @@ class BaseTest:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, request):
         options = webdriver.ChromeOptions()
-        #options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(options=options)
